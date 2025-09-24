3603ce0ac5f979e2f2255d5ed31bb7f64f8f711b
Rannamagica <fe.arriagadag@duocuc.cl>
2025-09-24 11:02:43 -0300

    Update logica.js

--- Workean2/match/logica.js
+++ Workean2/match/logica.js
@@ -2,9 +2,9 @@ const likeBtn = document.getElementById("likeBtn");
 const dislikeBtn = document.getElementById("dislikeBtn");
 
 likeBtn.addEventListener("click", () => {
-  alert("¡Te gusta esta imagen! ");
+  alert("¡Te gusta esta este trabajo ");
 });
 
 dislikeBtn.addEventListener("click", () => {
-  alert("No te gusta esta imagen ");
+  alert("No te gusta esta trabajo ");
 });
